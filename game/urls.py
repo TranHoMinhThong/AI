1f136e62c947cd51afa1aabe785c094b0f3fc111
Bui Quang Minh <minhquangbui99@hotmail.com>
2020-07-02 13:45:47 +0700

    Add redirect from / to play/

--- game/urls.py
+++ game/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('play/',include('play.urls')),
+    path('', lambda request: HttpResponseRedirect('/play')),
 ]
