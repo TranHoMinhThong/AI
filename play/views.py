f06119f6c8099a264f53bb55892db526dba24784
thuutri <53559948+thuutri@users.noreply.github.com>
2020-07-04 21:41:49 +0700

    Fix loi Thong gay ra :)))

--- play/views.py
+++ play/views.py
@@ -17,22 +17,26 @@ def tickCell(request):
         
         a=int(request.POST['a'])
         b=int(request.POST['b'])
-        cell=(a,b)
-        print(cell)
-        # check if alresdy win
-        state[0]=state[0].tickCell(1,cell)
-        print(state[0].status)
-        if state[0].status==1:
-            return JsonResponse(result(status=1).toJson(),safe=False)
+        if a != -1 and b != -1:
+            cell=(a,b)
+            print(cell)
+            # check if alresdy win
+            state[0]=state[0].tickCell(1,cell)
+            print(state[0].status)
+            if state[0].status==1:
+                return JsonResponse(result(status=1).toJson(),safe=False)
 
-        node=Node(cell)
-        MinRobot(state[0],node,0,3)
-        cell=node.children[0].cell
-        state[0]=state[0].tickCell(-1,cell)
-        if state[0].status==-1:
-            return JsonResponse(result(status=-1,robotCell=cell).toJson(),safe=False)
-        
-        node=Node(cell)
-        MaxHuman(state[0],node,0,3)
-        return JsonResponse(result(status=0,robotCell=cell,node=node).toJson(),safe=False)
+            node=Node(cell)
+            MinRobot(state[0],node,0,3)
+            cell=node.children[0].cell
+            state[0]=state[0].tickCell(-1,cell)
+            if state[0].status==-1:
+                return JsonResponse(result(status=-1,robotCell=cell).toJson(),safe=False)
+            
+            node=Node(cell)
+            MaxHuman(state[0],node,0,3)
+            return JsonResponse(result(status=0,robotCell=cell,node=node).toJson(),safe=False)
+        else:
+            state[0]=State()
+            return JsonResponse(result(status=1).toJson(),safe=False)
     
